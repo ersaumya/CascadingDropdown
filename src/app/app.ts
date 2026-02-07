9c2575b6af5514a077549c3199b0cc669060762e
SOUMYA\ersau <ersaumyarout@gmail.com>
2026-02-07 17:04:00 +0530

    Add department and designation dropdowns with API integration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, Signal, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 export interface State {
@@ -10,16 +11,34 @@ export interface City {
   stateId: string;
   cityName: string;
 }
+export interface Department {
+  departmentId: number;
+  departmentName: string;
+}
+
+export interface Designation {
+  designationId: number;
+  departmentId: number;
+  designationName: string;
+}
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('dependent-dropdown');
+export class App implements OnInit {
+  http = inject(HttpClient);
   selectedState: string = '';
   filteredCity: City[] = [];
+
+  ngOnInit() {
+    this.getAllDepartment();
+  }
+
+  departmentList = signal<Department[]>([]);
+  designationList = signal<Designation[]>([]);
+
   stateData: State[] = [
     { stateId: '1', stateName: 'Maharashtra' },
     { stateId: '2', stateName: 'Gujarat' },
@@ -40,5 +59,27 @@ export class App {
 
   onStateChange() {
     this.filteredCity = this.cityDate.filter((city) => city.stateId == this.selectedState);
+  }
+
+  getAllDepartment() {
+    this.http.get('https://api.freeprojectapi.com/api/EmployeeApp/GetDepartments').subscribe({
+      next: (res: any) => {
+        this.departmentList.set(res);
+      },
+    });
+  }
+
+  getDesignationByDeptId(event: any) {
+    debugger;
+    const deptId = event.target.value;
+    this.http
+      .get(
+        'https://api.freeprojectapi.com/api/EmployeeApp/GetDesignationsByDeptId?deptId=' + deptId,
+      )
+      .subscribe({
+        next: (res: any) => {
+          this.designationList.set(res);
+        },
+      });
   }
 }
